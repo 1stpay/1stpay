11202c70cbd28257d2aa7d2383a380586eecd36b
Nazim Medzhidov <nazim@medzhidov.ru>
2025-03-13 14:55:49 +0300

    added payment repository

---
+++ internal/repository/payment_repository.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"github.com/1stpay/1stpay/internal/model"
+	"gorm.io/gorm"
+)
+
+type PaymentRepository struct {
+	db *gorm.DB
+}
+
+type PaymentRepositoryInterface interface {
+	Create(payment model.Payment) (model.Payment, error)
+}
+
+func NewPaymentRepository(db *gorm.DB) *PaymentRepository {
+	return &PaymentRepository{
+		db: db,
+	}
+}
+
+func (r *PaymentRepository) Create(payment model.Payment) (model.Payment, error) {
+	if err := r.db.Create(&payment).Error; err != nil {
+		return model.Payment{}, err
+	}
+	return payment, nil
+}
