34ccd81ca732f7571a6eadaa236ef5a0b234bc72
Nazim Medzhidov <nazim@medzhidov.ru>
2025-03-13 20:22:25 +0300

    models update

--- internal/model/payment_model.go
+++ internal/model/payment_model.go
@@ -8,37 +8,34 @@ import (
 )
 
 type Payment struct {
-	ID                 uuid.UUID  `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
-	CreatedAt          time.Time  `gorm:"not null;default:now()"`
-	UpdatedAt          time.Time  `gorm:"not null;default:now()"`
-	TokenID            uuid.UUID  `gorm:"type:uuid;not null"`
-	Token              Token      `gorm:"foreignKey:TokenID"`
-	BlockchainID       uuid.UUID  `gorm:"type:uuid;not null"`
-	Blockchain         Blockchain `gorm:"foreignKey:BlockchainID"`
-	MerchantID         uuid.UUID  `gorm:"type:uuid;not null"`
-	Merchant           Merchant   `gorm:"foreignKey:MerchantID"`
-	RequestedAmount    float64    `gorm:"type:numeric(20,8);not null;default:0"`
-	PaidAmount         float64    `gorm:"type:numeric(20,8);not null;default:0"`
-	RequestedAmountWei int64      `gorm:"not null;default:0"`
-	PaidAmountWei      int64      `gorm:"not null;default:0"`
-	StableAmount       float64    `gorm:"type:numeric(20,8);not null"`
-	CommissionAmount   float64    `gorm:"type:numeric(20,8);not null;default:0"`
-	ExpireDate         *time.Time
-	AMLStatus          enum.PaymentAMLStatus `gorm:"type:payment_aml_status"`
-	Status             enum.PaymentStatus    `gorm:"type:payment_status;not null;default:'pending'"`
-	InvoiceEmail       string
+	ID               uuid.UUID `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
+	CreatedAt        time.Time `gorm:"not null;default:now()"`
+	UpdatedAt        time.Time `gorm:"not null;default:now()"`
+	MerchantID       uuid.UUID `gorm:"type:uuid;not null"`
+	Merchant         Merchant  `gorm:"foreignKey:MerchantID"`
+	RequestedAmount  float64   `gorm:"type:numeric(20,8);not null;default:0"`
+	PaidAmount       float64   `gorm:"type:numeric(20,8);not null;default:0"`
+	CommissionAmount float64   `gorm:"type:numeric(20,8);not null;default:0"`
+	ExpiresAt        *time.Time
+	AMLStatus        *enum.PaymentAMLStatus `gorm:"type:payment_aml_status"`
+	Status           enum.PaymentStatus     `gorm:"type:payment_status;not null;default:'pending'"`
+	InvoiceEmail     *string
+	UsedTokenID      *uuid.UUID `gorm:"type:uuid;"`
+	UsedToken        *Merchant  `gorm:"foreignKey:UsedTokenID"`
 }
 
 type PaymentAddress struct {
-	ID           uuid.UUID  `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
-	CreatedAt    time.Time  `gorm:"not null;default:now()"`
-	UpdatedAt    time.Time  `gorm:"not null;default:now()"`
-	PaymentID    uuid.UUID  `gorm:"type:uuid;not null"`
-	Payment      Payment    `gorm:"foreignKey:PaymentID"`
-	BlockchainID uuid.UUID  `gorm:"type:uuid;not null"`
-	Blockchain   Blockchain `gorm:"foreignKey:BlockchainID"`
-	TokenID      uuid.UUID  `gorm:"type:uuid;not null"`
-	Token        Token      `gorm:"foreignKey:TokenID"`
-	PublicKey    string     `gorm:"not null"`
-	PrivateKey   string     `gorm:"not null"`
+	ID                 uuid.UUID `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
+	CreatedAt          time.Time `gorm:"not null;default:now()"`
+	UpdatedAt          time.Time `gorm:"not null;default:now()"`
+	PaymentID          uuid.UUID `gorm:"type:uuid;not null"`
+	Payment            Payment   `gorm:"foreignKey:PaymentID"`
+	TokenID            uuid.UUID `gorm:"type:uuid;not null"`
+	Token              Token     `gorm:"foreignKey:TokenID"`
+	PublicKey          string    `gorm:"not null"`
+	PrivateKey         string    `gorm:"not null"`
+	RequestedAmount    float64   `gorm:"type:numeric(20,8);not null;default:0"`
+	PaidAmount         float64   `gorm:"type:numeric(20,8);not null;default:0"`
+	RequestedAmountWei int       `gorm:"type:bigint;not null;default:0"`
+	PaidAmountWei      int       `gorm:"type:bigint;not null;default:0"`
 }
